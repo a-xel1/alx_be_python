70882fa06faa56e21213cb4802bb108b653a2d2e
a-xel1 <133992614+a-xel1@users.noreply.github.com>
2025-09-13 11:39:07 +0000

    Create basic_operations.py

---
+++ basic_operations.py
@@ -0,0 +1,9 @@
+numb1 = 10
+numb2 = 5
+sum = numb1 + numb2
+difference = numb1 - numb2
+product = numb1 * numb2
+
+print(f"Addition of {numb1} and {numb2} is {sum}")
+print(f"Subtracion of {numb1} and {numb2} is {difference}")
+print(f"Multiplication of {numb1} and {numb2} is {product}")
